0f555748fa9fdbde82250f60e520233f1a6ae725
Scott A. Matthews <scott@designsbysm.com>
2023-07-04 10:15:54 -0700

    set improve test coverage

--- set/add_test.go
+++ set/add_test.go
@@ -15,3 +15,18 @@ func TestAdd(t *testing.T) {
 		t.Errorf("should have 2, got %d", size)
 	}
 }
+
+func TestAddEmpty(t *testing.T) {
+	set := New[string]()
+	set.items = nil
+
+	if length := set.Length(); length != 0 {
+		t.Errorf("should be 0, got %d", length)
+	}
+
+	set.Add("item1")
+	set.Add("item2")
+	if size := set.Length(); size != 2 {
+		t.Errorf("should have 2, got %d", size)
+	}
+}
--- set/items_test.go
+++ set/items_test.go
@@ -14,8 +14,8 @@ func TestItems(t *testing.T) {
 		t.Errorf("should have 3, got %d", len(items))
 	}
 
-	found := items[1]
-	if found != "item2" {
-		t.Errorf("should have item2, got %s", found)
+	found := set.Has("item2")
+	if !found {
+		t.Errorf("should have item2")
 	}
 }
